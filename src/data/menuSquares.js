b1fe2934f8418f9a85dfb9753d88aa8994ce606f
Jamie.Nisbet <jamie.nisbet@apoyar.eu>
2023-04-18 02:34:57 +0200

    first commit

---
+++ src/data/menuSquares.js
@@ -0,0 +1,27 @@
+export const menuSquares = [
+    {
+      id: 1,
+      link: '/hotel-info',
+      image: '/hotel.jpg',
+      title: 'Hotel Information',
+    },
+    {
+      id: 2,
+      link: '/golf',
+      image: '/golf.jpg',
+      title: 'Golf',
+    },
+    {
+      id: 3,
+      link: '/local-activities',
+      image: '/activities.jpg',
+      title: 'Activities',
+    },
+    {
+        id: 4,
+        link: '/feedback',
+        image: '/feedback.jpg',
+        title: 'Feedback',
+      },
+  ];
+  
---
+++ src/templates/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <title>Pierpont</title>
+      </head>
+  );
+}
+
+export default Header;
