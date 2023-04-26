5a524c16f3865c776ddf5023e4b49b1e02c0181e
Florhen <131691766+Florhen@users.noreply.github.com>
2023-04-26 15:01:04 +0000

    data update

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -31,12 +31,10 @@ export default function Welcome() {
           </div>
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Welcome
+            BIENVENUE • WELKOM • WELCOME
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-              fugiat aliqua.
+            Golf & Hotel de Pierpont
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
