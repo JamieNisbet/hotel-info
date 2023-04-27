0757b8dcd4989e554c65f215c54b13ed105dbf76
Jamie.Nisbet <jamie.nisbet@apoyar.eu>
2023-04-27 09:22:16 +0200

    bg image fix

--- src/templates/Header.jsx
+++ src/templates/Header.jsx
@@ -16,8 +16,8 @@ export default function Example() {
     <div className="bg-white">
       <header className="absolute inset-x-0 top-0 z-50">
         <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-          <div className="flex lg:flex-1">
-            <a href="/" className="-m-1.5 p-1.5">
+          <div className="flex lg:flex-1 ">
+            <a href="/" className="-m-1.5 p-1.5 bg-white rounded-full w-fit">
               <span className="sr-only">Your Company</span>
               <img
                 className="h-24 w-auto"
@@ -29,7 +29,7 @@ export default function Example() {
           <div className="flex lg:hidden">
             <button
               type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 bg-white text-gray-700"
               onClick={() => setMobileMenuOpen(true)}
             >
               <span className="sr-only">Open main menu</span>
@@ -38,13 +38,13 @@ export default function Example() {
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+              <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 bg-white p-2 px-5 hover:bg-gray-700 hover:text-white rounded-xl text-gray-900">
                 {item.name}
               </a>
             ))}
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            <a href="#" className="text-sm font-semibold leading-6 bg-white rounded-xl p-2 text-gray-900">
               We want to hear from you <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,7 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-        backgroundImage: {
-          'hero-pattern': "url('/img/hotel-pierpont-welcome.jpeg')",
-          'footer-texture': "url('/img/footer-texture.png')",
-        }
-    },
+    extend: {},
   },
   plugins: [],
 }
